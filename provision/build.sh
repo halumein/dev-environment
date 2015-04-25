672b5decd75aae0e738bb6e9419741834b6525e1
Тришкин Вадим <trishkin_vo@taximaxim.ru>
2015-04-25 17:47:32 +0500

    Initial commit

---
+++ provision/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PROVISION='/vagrant/provision'
+
+cat ${PROVISION}/mysql.preseed | sudo debconf-set-selections
+cat ${PROVISION}/phpmyadmin.preseed | sudo debconf-set-selections
+sudo cp ${PROVISION}/apache2.conf /etc/apache2/apache2.conf
+
+sudo apt-get install -y mc apache2 mysql-server php5 phpmyadmin php5-apcu php5-memcache
