e9ffca45f9481ac899191f04d39dbe256ecdd6de
Тришкин Вадим <trishkin_vo@taximaxim.ru>
2015-04-28 10:58:51 +0500

    fix build sh

--- provision/build.sh
+++ provision/build.sh
@@ -7,3 +7,5 @@ cat ${PROVISION}/phpmyadmin.preseed | sudo debconf-set-selections
 sudo cp ${PROVISION}/apache2.conf /etc/apache2/apache2.conf
 
 sudo apt-get install -y mc apache2 mysql-server php5 phpmyadmin php5-apcu php5-memcache
+sudo locale-gen ru_RU.UTF-8
+sudo update-locale
