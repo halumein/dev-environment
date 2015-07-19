639867bf06830cf1f129b0349d1c48c27cd610d2
Тришкин Вадим <trishkin_vo@taximaxim.ru>
2015-07-19 15:55:52 +0500

    some fix

--- provision/build.sh
+++ provision/build.sh
@@ -6,6 +6,8 @@ cat ${PROVISION}/mysql.preseed | sudo debconf-set-selections
 cat ${PROVISION}/phpmyadmin.preseed | sudo debconf-set-selections
 sudo cp ${PROVISION}/apache2.conf /etc/apache2/apache2.conf
 
+sudo apt-get update
 sudo apt-get install -y mc apache2 mysql-server php5 phpmyadmin php5-apcu php5-memcache
 sudo locale-gen ru_RU.UTF-8
 sudo update-locale
+sudo a2enmode rewrite
