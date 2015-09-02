a25dc20e7204c728a9787eb62f57e3a7da39ef9a
Halumein <halumein@gmail.com>
2015-09-02 23:03:20 +0300

    add git, node, npm, bower

--- provision/build.sh
+++ provision/build.sh
@@ -7,7 +7,8 @@ cat ${PROVISION}/phpmyadmin.preseed | sudo debconf-set-selections
 sudo cp ${PROVISION}/apache2.conf /etc/apache2/apache2.conf
 
 sudo apt-get update
-sudo apt-get install -y mc apache2 mysql-server php5 phpmyadmin php5-apcu php5-memcache
+sudo apt-get install -y mc apache2 mysql-server php5 phpmyadmin php5-apcu php5-memcache git npm npm-legacy
 sudo locale-gen ru_RU.UTF-8
 sudo update-locale
 sudo /usr/sbin/a2enmod rewrite
+sudo npm install -g bower
